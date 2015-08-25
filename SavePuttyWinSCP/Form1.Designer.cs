a5fe05bf676326de3bb06cac94bc65deda716df4
Daniel Csaba <dev@csdhome.info>
2015-08-25 17:14:14 +0200

    First commit.

---
+++ SavePuttyWinSCP/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace SavePuttyWinSCP
+{
+    partial class ExportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExpPutty = new System.Windows.Forms.Button();
+            this.btnExpWinSCP = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExpPutty
+            // 
+            this.btnExpPutty.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExpPutty.Location = new System.Drawing.Point(84, 12);
+            this.btnExpPutty.Name = "btnExpPutty";
+            this.btnExpPutty.Size = new System.Drawing.Size(201, 25);
+            this.btnExpPutty.TabIndex = 0;
+            this.btnExpPutty.Text = "Export Putty";
+            this.btnExpPutty.UseVisualStyleBackColor = true;
+            this.btnExpPutty.Click += new System.EventHandler(this.btnExpPutty_Click);
+            // 
+            // btnExpWinSCP
+            // 
+            this.btnExpWinSCP.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExpWinSCP.Location = new System.Drawing.Point(84, 43);
+            this.btnExpWinSCP.Name = "btnExpWinSCP";
+            this.btnExpWinSCP.Size = new System.Drawing.Size(201, 23);
+            this.btnExpWinSCP.TabIndex = 1;
+            this.btnExpWinSCP.Text = "Export WinSCP";
+            this.btnExpWinSCP.UseVisualStyleBackColor = true;
+            this.btnExpWinSCP.Click += new System.EventHandler(this.btnExpWinSCP_Click);
+            // 
+            // ExportForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 80);
+            this.Controls.Add(this.btnExpWinSCP);
+            this.Controls.Add(this.btnExpPutty);
+            this.MaximizeBox = false;
+            this.Name = "ExportForm";
+            this.Text = "Export Putty / WinSCP";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExpPutty;
+        private System.Windows.Forms.Button btnExpWinSCP;
+    }
+}
+
---
+++ SavePuttyWinSCP/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SavePuttyWinSCP
+{
+    public partial class ExportForm : Form
+    {
+        private SaveFileDialog savePutty;
+        private SaveFileDialog saveWinSCP;
+
+        public ExportForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExpPutty_Click(object sender, EventArgs e)
+        {
+            savePutty = new SaveFileDialog();
+            savePutty.Title = "Save Registry File";
+            savePutty.FileName = "Putty.reg";
+            savePutty.Filter = "Registry files|*.reg";
+            savePutty.InitialDirectory = @"C:\";
+            if (savePutty.ShowDialog() == DialogResult.OK)
+            {
+
+                String fileName = savePutty.FileName.ToString();
+                String cmd = "/e \""+ fileName + "\" HKEY_CURRENT_USER\\Software\\SimonTatham";
+
+                System.Diagnostics.Process regeditProcess = System.Diagnostics.Process.Start("regedit.exe", cmd);
+                regeditProcess.WaitForExit();
+
+
+                MessageBox.Show("Registry Exported.");
+                Console.WriteLine(cmd);
+            }
+        }
+
+        private void btnExpWinSCP_Click(object sender, EventArgs e)
+        {
+            saveWinSCP = new SaveFileDialog();
+            saveWinSCP.Title = "Save Registry File";
+            saveWinSCP.FileName = "WinSCP.reg";
+            saveWinSCP.Filter = "Registry files|*.reg";
+            saveWinSCP.InitialDirectory = @"C:\";
+            if (saveWinSCP.ShowDialog() == DialogResult.OK)
+            {
+
+                String fileName = saveWinSCP.FileName.ToString();
+                String cmd = "/e \"" + fileName + "\" \"HKEY_CURRENT_USER\\Software\\Martin Prikryl\"";
+
+                System.Diagnostics.Process regeditProcess = System.Diagnostics.Process.Start("regedit.exe", cmd);
+                regeditProcess.WaitForExit();
+
+
+                MessageBox.Show("Registry Exported.");
+                Console.WriteLine(cmd);
+            }
+        }
+    }
+}
